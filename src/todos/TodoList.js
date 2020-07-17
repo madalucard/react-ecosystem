c54ddbb2450f92c6927f29d9dc99d9ce2a88dbfa
Martin Lapihuska <martin.lapihuska@quadrotech-it.com>
2020-07-17 12:46:47 +0200

    ch02e09 => todo list item with buttons

---
+++ src/todos/TodoList.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import TodoListItem from '.TodoListItem';
+
+const TodoList = ({ todos }) => (
+	<div className='list-wraper'>
+		{todos.map((todo) => (
+			<TodoListItem todo={todo} />
+		))}
+	</div>
+);
+
+export default TodoList;
